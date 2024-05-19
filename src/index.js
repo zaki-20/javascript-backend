4c240a7f591c80c446763e48f1bfbb4c3578ce4a
zaki-20 <zakibutt199@gmail.com>
2024-05-20 04:40:44 +0500

    important middlewares added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-// import {app} from './app.js'
+import {app} from './app.js'
 dotenv.config({
     path: './.env'
 })
@@ -8,14 +8,14 @@ dotenv.config({
 
 
 connectDB()
-// .then(() => {
-//     app.listen(process.env.PORT || 8000, () => {
-//         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-//     })
-// })
-// .catch((err) => {
-//     console.log("MONGO db connection failed !!! ", err);
-// })
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
 
 
 
