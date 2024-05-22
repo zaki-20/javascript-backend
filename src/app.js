cca45d278cc4c816e4bcc6e2c2ee739338d15c90
zaki-20 <zakibutt199@gmail.com>
2024-05-22 05:37:16 +0500

    Register controller

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 app.use(helmet());
-app.use(morgan('combined'));
+app.use(morgan());
 app.use(compression());
 
 
